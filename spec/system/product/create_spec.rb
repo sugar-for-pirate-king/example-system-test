53fbed4f093ce42885c74dbb464faa0df71c2e86
philiplambok <philiplambok71@gmail.com>
2019-02-24 13:23:11 +0700

    refactor spec

--- spec/system/product/create_spec.rb
+++ spec/system/product/create_spec.rb
@@ -3,16 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Create Product', type: :system do
+  before do
+    visit new_product_path
+  end
+
   context 'with valid params' do
     it 'should create product' do
-      visit new_product_path
-
-      within '#product-form' do
-        fill_in :product_name, with: 'Kaos'
-        fill_in :product_price, with: '2000000'
-        click_button 'Submit'
-      end
-
+      submit_product_form(name: 'Kaos', price: '2000000')
       expect(page).to have_text 'Product has been created'
       expect(page).to have_text 'Kaos', '2000000'
     end
@@ -21,30 +18,25 @@ RSpec.describe 'Create Product', type: :system do
   context 'with invalid params' do
     context 'without name' do
       it 'should returns error' do
-        visit new_product_path
-
-        within '#product-form' do
-          fill_in :product_name, with: nil
-
-          click_button 'Submit'
-        end
-
+        submit_product_form(name: nil)
         expect(page).to have_text "Name can't be blank"
       end
     end
 
     context 'without price' do
       it 'should returns error' do
-        visit new_product_path
-
-        within '#product-form' do
-          fill_in :product_price, with: nil
-
-          click_button 'Submit'
-        end
-
+        submit_product_form(price: nil)
         expect(page).to have_text "Price can't be blank"
       end
     end
+  end
+end
+
+def submit_product_form(params = {})
+  within '#product-form' do
+    fill_in :product_name, with: params[:name]
+    fill_in :product_price, with: params[:price]
+
+    click_button 'Submit'
   end
 end
